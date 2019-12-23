18a9ebde1b530a0fa96f0632aafafe0160bd0ab5
Mohamed Bengezi <bengezim@mcmaster.ca>
2019-12-23 09:24:35 -0500

    progress

--- webdriver_basics.py
+++ webdriver_basics.py
@@ -24,9 +24,10 @@ def gecko_test(site_000='https://shop.misha-and-puff.com/products/house-sweater?
     # elem = driver.find_element_by_link_text('House Sweater')
     # print(elem)
     # elem.send_keys(webdriver.common.keys.Keys.RETURN)
-    sleep(3)
+    sleep(2)
 
-    select = driver.find_element_by_xpath("//select[@data-product-option='option2']")
+    select = driver.find_element_by_xpath(
+        "//select[@data-product-option='option2']")
     all_options = select.find_elements_by_tag_name("option")
     for option in all_options:
         value = option.get_attribute("value")
@@ -43,24 +44,32 @@ def gecko_test(site_000='https://shop.misha-and-puff.com/products/house-sweater?
 
     checkout = driver.find_element_by_xpath("//a[@href='/checkout']")
     checkout.send_keys(webdriver.common.keys.Keys.RETURN)
-    sleep(2)
+    sleep(4)
 
-    email = driver.find_element_by_xpath("//input[@id='checkout_email']")
-    email.send_keys('testst')
+    email = driver.find_element_by_id("checkout_email")
+    email.send_keys('hanin68@gmail.com')
 
-    firstName = driver.find_element_by_xpath("//input[@name='checkout[shipping_address][first_name]']")
+    firstName = driver.find_element_by_id(
+        "checkout_shipping_address_first_name")
     firstName.send_keys('Hanin')
 
-    lastName = driver.find_element_by_xpath("//input[@id='checkout_shipping_address_last_name']")
+    lastName = driver.find_element_by_id("checkout_shipping_address_last_name")
     lastName.send_keys('Bengezi')
 
-    address = driver.find_element_by_xpath("//input[@id='checkout_shipping_address_address1']")
-    address.send_keys('12 testing st')
+    address = driver.find_element_by_id(
+        "checkout_shipping_address_address1")
+    address.send_keys('6818 137th Avenue Northeast')
 
-    city = driver.find_element_by_xpath("//input[@id='checkout_shipping_address_city']")
-    city.send_keys('Seattle')
+    aprtment = driver.find_element_by_id(
+        "checkout_shipping_address_address2")
+    aprtment.send_keys('437')
 
-    country = driver.find_element_by_xpath("//select[@id='checkout_shipping_address_country']")
+    city = driver.find_element_by_id(
+        "checkout_shipping_address_city")
+    city.send_keys('Redmond')
+
+    country = driver.find_element_by_id(
+        "checkout_shipping_address_country")
     all_options = country.find_elements_by_tag_name("option")
     for option in all_options:
         value = option.get_attribute("value")
@@ -69,7 +78,8 @@ def gecko_test(site_000='https://shop.misha-and-puff.com/products/house-sweater?
             option.click()
             break
 
-    state = driver.find_element_by_xpath("//select[@id='checkout_shipping_address_province']")
+    state = driver.find_element_by_id(
+        "checkout_shipping_address_province")
     state_options = state.find_elements_by_tag_name("option")
     for states in state_options:
         value1 = states.get_attribute("value")
@@ -79,15 +89,42 @@ def gecko_test(site_000='https://shop.misha-and-puff.com/products/house-sweater?
             states.click()
             break
 
-    zipcode = driver.find_element_by_xpath("//input[@id='checkout_shipping_address_zip']")
-    zipcode.send_keys('TEST')
+    zipcode = driver.find_element_by_id(
+        "checkout_shipping_address_zip")
+    zipcode.send_keys('98052')
 
-    phone = driver.find_element_by_xpath("//input[@id='checkout_shipping_address_phone']")
+    phone = driver.find_element_by_id("checkout_shipping_address_phone")
     phone.send_keys('9055311442')
 
-    button =  driver.find_element_by_xpath("//button[@name='button']")
+    button = driver.find_element_by_xpath("//button[@name='button']")
     button.send_keys(webdriver.common.keys.Keys.RETURN)
 
+    sleep(1)
+
+    try:
+        proceed = driver.find_element_by_id("btn-proceed-address")
+        proceed.send_keys(webdriver.common.keys.Keys.RETURN)
+    except:
+        print("Couldn't locate proceed button")
+
+    sleep(2.5)
+
+    cont = driver.find_element_by_name("button")
+    cont.send_keys(webdriver.common.keys.Keys.RETURN)
+
+    sleep(2.5)
+
+    cardNumber = driver.find_element_by_id("number")
+    cardNumber.send_keys('9055311442')
+
+    name = driver.find_element_by_id("name")
+    name.send_keys('9055311442')
+
+    expiry = driver.find_element_by_id("expiry")
+    expiry.send_keys('9055311442')
+
+    verification_value = driver.find_element_by_id("verification_value")
+    verification_value.send_keys('9055311442')
 
     # driver.find_element_by_xpath("//input[@id='checkout_shipping_address_first_name']")
     # checkout_shipping_address_first_name
@@ -100,11 +137,8 @@ def gecko_test(site_000='https://shop.misha-and-puff.com/products/house-sweater?
     # checkout_shipping_address_phone
     # button continue_to_shipping_method_button
 
-
     # k, cool. let's bounce.
   #  driver.quit()
-
-
 # make runable
 if __name__ == '__main__':
     # here we go
