72c93093ae6497ae84a0fa937f228f7b5524c3d5
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-05-11 17:37:45 -0400

    Changing for different site

--- clothingBot.py
+++ clothingBot.py
@@ -6,46 +6,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+
+site_000 = 'https://apolina-kids.com/collections/all'
 
 
-site_000 = 'https://shop.misha-and-puff.com/collections/sale'
-
-
-def selectSize():
-    try:
-        select = driver.find_element_by_xpath(
-            "//select[@data-product-option='option2']")
-        all_options = select.find_elements_by_tag_name("option")
-        for option in all_options:
-            value = option.get_attribute("value")
-            if value == "4-5y":
-                print("Value is: %s" % value)
-                option.click()
-    except:
-        print('No select found')
-
-
-def addToCart():
-    addToCart = driver.find_element_by_xpath("//input[@value='Add to cart']")
-
-    try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "add-to-cart"))
-        )
-    except:
-        print("timed out waiting for product page")
-
-    try:
-        addToCart.send_keys(webdriver.common.keys.Keys.RETURN)
-    except:
-        try:
-            addToCart.click()
-        except:
-            print("Could not click 'Add to cart'")
-    sleep(2)
-
-    checkout = driver.find_element_by_xpath("//a[@href='/checkout']")
-    checkout.send_keys(webdriver.common.keys.Keys.RETURN)
+if __name__ == '__main__':
+    # setting the site and driver
+    driver = webdriver.Firefox()
+    # load the site
+    URL = site_000
+    driver.get(URL)
+    sleep(1)
+    findItem('POL DRESS - FARM CHECK / HAY')
+    sleep(1)
+    selectSize()
+    addToCart()
+    sleep(3)
+    shippingDetails()
+    clickButton(None)
+    sleep(2.5)
+    clickButton(None)
+    sleep(3)
 
 
 def findItem(prodName):
@@ -56,30 +38,47 @@ def findItem(prodName):
     action.perform()
 
 
-def clickButton():
-    cont = driver.find_element_by_name("button")
+def selectSize():
+    try:
+        select = driver.find_element_by_xpath(
+            "//select[@id=\"product-select-4540644753485-option-0\"]")
+        all_options = select.find_elements_by_tag_name("option")
+        for option in all_options:
+            value = option.get_attribute("value")
+            if value == "5-7Y":
+                print("Value is: %s" % value)
+                option.click()
+    except:
+        print('No select found')
+
+
+def addToCart():
+    try:
+        addToCart = driver.find_element_by_xpath(
+            "//input[@value='Add to Cart']")
+    except:
+        print('Add To Cart button not found')
+
+    try:
+        addToCart.send_keys(webdriver.common.keys.Keys.RETURN)
+    except:
+        try:
+            addToCart.click()
+        except:
+            print("Could not click 'Add to cart'")
+    sleep(2)
+
+    checkout = driver.find_element_by_xpath(
+        "//input[@value='Check Out']")
+    checkout.send_keys(webdriver.common.keys.Keys.RETURN)
+
+
+def clickButton(id):
+    if (id is None):
+        cont = driver.find_element_by_name("button")
+    else:
+        cont = driver.find_element_by_id(id)
     cont.send_keys(webdriver.common.keys.Keys.RETURN)
-
-
-def inputPayment():
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.frame_to_be_available_and_switch_to_it(
-        (By.CLASS_NAME, "card-fields-iframe")))
-
-    cardNumber = driver.find_element_by_id("number")
-    cardNumber.send_keys('493')
-
-    name = driver.find_element_by_id("name")
-    wait.until(EC.element_to_be_clickable((By.ID, "name")))
-    name.click()
-
-    expiry = driver.find_element_by_id("expiry")
-    expiry.send_keys('04 / 34')
-
-    verification_value = driver.find_element_by_id("verification_value")
-    verification_value.send_keys('123')
-
-    driver.switch_to.default_content()
 
 
 def shippingDetails():
@@ -134,21 +133,37 @@ def shippingDetails():
     phone.send_keys('9055311442')
 
 
-if __name__ == '__main__':
-    # setting the site and driver
-    driver = webdriver.Firefox()
-    # load the site
-    URL = sys.argv[1]
-    driver.get(URL)
-    # sleep(1)
-    # findItem(prodName)
-    sleep(1)
-    selectSize()
-    addToCart()
-    sleep(6)
-    shippingDetails()
-    clickButton()
-    sleep(4.5)
-    clickButton()
-    sleep(7)
-    inputPayment()
+# def inputPayment():
+    # driver.switch_to.frame(driver.find_element_by_xpath(
+    #     "//*[contains(@id,'card-fields-number')]"))
+    # wait = WebDriverWait(driver, 10)
+    # wait.until(EC.frame_to_be_available_and_switch_to_it(
+    #     (By.CLASS_NAME, "card-fields-iframe")))
+
+    # cardNumber = driver.find_element_by_id("number")
+    # cardNumber.send_keys('4930 0000 0000 0000')
+
+    # name = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+    #     (By.XPATH, "//input[@id='name']")))
+    # driver.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);",
+    #                       name,
+    #                       "value",
+    #                       "NNAAAAMME")
+    # name.send_keys('NAME')
+    # name.send_keys(webdriver.common.keys.Keys.RETURN)
+
+    # js = "arguments[0].setAttribute('value','\"+NAME+\"')"
+    # expiry = driver.find_element_by_id("expiry")
+    # driver.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);",
+    #                       expiry,
+    #                       "value",
+    #                       "04 / 34")
+
+    # verification_value = driver.find_element_by_id("verification_value")
+    # driver.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);",
+    #                       verification_value,
+    #                       "value",
+    #                       "123")
+    # sleep(10)
+
+    # driver.switch_to.default_content()
